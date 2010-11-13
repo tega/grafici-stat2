d06ef09e342f34ce1e8e369b6d0f25b9b5492b47
Claudio <claudio.ortelli@gmail.com>
2010-11-13 17:55:06 +0100

    continuato modifica test

--- codiceR/convergenzaInProbabilita2.R
+++ codiceR/convergenzaInProbabilita2.R
@@ -4,35 +4,47 @@
 ###############################################################################
 
 
-#pdf(file="densityEstimatorBiasedUnBiased.pdf"#
-#		,width=12,height=10)
-f <- function(x) {
-	exp(-1/8*(x-1)^2)/sqrt(4*pi)
+pdf(file="grafici/convergenzaInProbabilita2.pdf")
+f1 <- function(x) {
+	sigma = 0.5
+	mu = 1.5
+	exp(-1/2*(x-mu)^2/sigma^2)/sqrt(2*pi)/sigma
 }
-
+f2 <- function(x) {
+	sigma = 0.8
+	mu = -1.5
+	exp(-1/2*(x-mu)^2/sigma^2)/sqrt(2*pi)/sigma
+}
+h1 <- function(x) {
+	sigma = 1
+	mu = 0.7
+	exp(-1/2*(x-mu)^2/sigma^2)/sqrt(2*pi)/sigma
+}
+h2 <- function(x) {
+	sigma = 1.4
+	mu = 0.-0.8
+	exp(-1/2*(x-mu)^2/sigma^2)/sqrt(2*pi)/sigma
+}
 g <- function(x) {
-	exp(-(x-0.5)^2)*sqrt(2/pi)
+	sigma = 0.6
+	mu = 0
+	exp(-1/2*(x-mu)^2/sigma^2)/sqrt(2*pi)/sigma
 }
-
-h <- function(x) {
-	exp(-2*x^2)*4/sqrt(2*pi)
-}
-
 x <- seq(-4,4,length.out = 100)
-y1 <- g(x)
-y2 <- f(x)
-y3 <- h(x)
+y1 <- 0.8*f1(x)+0.2*f2(x)
+y2 <- 0.6*h1(x)+0.4*h2(x)
+y3 <- g(x)
 
-plot(x,y3,type="l",lty=3,axes=FALSE,
+plot(x,y1,type="l",lty=1,axes=FALSE,
 		xlab="",ylab="y")
-lines(x,y2)
-lines(x,y1,lty=2)
+lines(x,y2,lty=3)
+lines(x,y3,lty=2)
 Axis(at=-4:4,side=1, labels=c(rep("",4),expression(theta),rep("",4)))
 Axis(side=2,las=1)
 abline(v=0)
-text(0.8,1.3,expression(f[hat(theta)[20]]))
-text(1.6,0.5,expression(f[hat(theta)[10]]))
-text(3.5,0.2,expression(f[hat(theta)[5]]))
+text(0.6,0.55,expression(f[hat(theta)[20]]))
+text(1.6,0.2,expression(f[hat(theta)[10]]))
+text(2,0.55,expression(f[hat(theta)[5]]))
 
-#dev.off()
+dev.off()
 
